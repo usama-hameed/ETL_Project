8e28dc06f9159a5e10d39a1824b812d9a6440f66
usama-hameed <musamahameed97@gmail.com>
2024-02-15 06:39:26 +0100

    songs and artists improvments

--- src/main.py
+++ src/main.py
@@ -1,25 +1,30 @@
 from crawler import crawl_data
-# from sqlalchemy.orm import sessionmaker
-# from db.models import Event
-# from db.connection import engine
-#
-# Session = sessionmaker(bind=engine)
-# session = Session()
+from sqlalchemy.orm import sessionmaker
+from db.models import Event, Song
+from db.connection import engine
+
+
+Session = sessionmaker(bind=engine)
+session = Session()
 
 URL = 'https://www.lucernefestival.ch/en/program/summer-festival-24'
 
 
-# def save_data(data):
-#     event = Event(title=data['title'], date=data['date'], time=data['time'],
-#                   location=data['time'], image_url=data['image_url'])
-#     session.add(event)
-#     session.commit()
-#     session.close()
+def save_data(data):
+    event = Event(title=data.get('title', None), date=data.get('date', None), time=data.get('time', None),
+                  location=data.get('location', None), image_url=data.get('image_link', None))
+    session.add(event)
+
+    if 'songs' in data:
+        for song_info in data['songs']:
+            song = Song(name=song_info.get('song', None), composer=song_info.get('composer', None), event=event)
+            session.add(song)
+
+    session.commit()
+    session.close()
 
 
 events_data = crawl_data(url=URL)
 
-# for data in events_data:
-#     print("------------------------------------------------------------")
-#     print(data)
-#     print("------------------------------------------------------------")
+for data in events_data:
+    save_data(data)
